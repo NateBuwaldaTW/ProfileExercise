1c76c8d24738fc60df41a80eda7a1d5db1b10559
Nathan Buwalda <nathan.buwalda@va.gov>
2023-08-04 07:43:10 -0400

    Initial commit

---
+++ src/main/java/com/thoughtworks/profileexercise/ProfileExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.profileexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thoughtworks/profileexercise/ProfileExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.profileexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
