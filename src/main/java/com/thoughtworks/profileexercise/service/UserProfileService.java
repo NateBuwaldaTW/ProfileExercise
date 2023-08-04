67f833c180536727c6e21e8ed6b6a42b23b1e671
Nathan Buwalda <nathan.buwalda@thoughtworks.com>
2023-08-04 11:14:34 -0400

    Add create profile service method

--- src/main/java/com/thoughtworks/profileexercise/service/UserProfileService.java
+++ src/main/java/com/thoughtworks/profileexercise/service/UserProfileService.java
@@ -1,4 +1,11 @@
 package com.thoughtworks.profileexercise.service;
 
+import com.thoughtworks.profileexercise.controller.UserProfile;
+
 public class UserProfileService {
+
+    public boolean createUserProfile(UserProfile profile) {
+        return null != profile;
+    }
+
 }
--- src/test/java/com/thoughtworks/profileexercise/service/UserProfileServiceTest.java
+++ src/test/java/com/thoughtworks/profileexercise/service/UserProfileServiceTest.java
@@ -1,15 +1,31 @@
 package com.thoughtworks.profileexercise.service;
 
+import com.thoughtworks.profileexercise.controller.UserProfile;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UserProfileServiceTest {
 
+    private UserProfileService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new UserProfileService();
+    }
+
     @Test
     void serviceIsInstantiated() {
-        var service = new UserProfileService();
-
         Assertions.assertNotNull(service);
+    }
+
+    @Test
+    void shouldCreateUserProfile() {
+        var expectedProfile = new UserProfile();
+
+        var actual = service.createUserProfile(expectedProfile);
+
+        Assertions.assertTrue(actual);
     }
 
 }
